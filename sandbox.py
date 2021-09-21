ea217a658bd862a27d795efd4b29a4acc4984840
Khandice <s.khandice@gmail.com>
2021-09-21 15:16:10 -0700

    this is our scratchwork sheet

---
+++ sandbox.py
@@ -0,0 +1,54 @@
+## WAVE 1
+
+
+def draw_letters():
+    #draws_ten
+    #is_list_of_letter_strings
+    #letter_not_selected_too_many_times
+    pass
+
+
+## WAVE 2
+
+def uses_available_letters(word, letters):
+    # true_word_in_letter_bank
+    # false_word_in_letter_bank
+    # false_word_overuses_letter
+    # does_not_change_letter_bank
+    pass
+
+
+## WAVE 3
+
+def test_score(score_word):
+    score_list = []
+    score = sum(score_list)
+    if score_word == "":
+        return 0
+    else:
+        for letter in score_word:
+            score_list.append(LETTER_POOL[letter.upper()])
+        #if len(score_word) >= 7:
+            #
+       
+    # word_accurate
+    # word_accurate_ignores_case
+    # zero_for_empty
+    # extra_points_for_seven_or_longer
+           
+   
+
+
+## WAVE 4
+
+def get_highest_word_score(words):
+    # accurate
+    # accurate_unsorted_list
+    # tie_prefers_shorter_word
+    # tie_prefers_shorter_word_unsorted_list
+    # tie_prefers_ten_letters
+    # tie_same_length_prefers_first
+    pass
+
+
+
