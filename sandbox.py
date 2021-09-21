c01a264e0fa1dc9c86171317cd2ce5c73adbb79d
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-09-21 16:11:12 -0700

    updates our collective notes

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,82 @@
 ## WAVE 1
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 
 def draw_letters():
+
+    import random 
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    letters=[]
+    letter_pool_lst=[]
+    tup=""
+
+    for letter, numby in LETTER_POOL.items():
+        tup= tuple(letter*numby)
+
+        for t in tup:
+            for x in t:
+                letter_pool_lst.append(x)
+
+    letters = random.sample(letter_pool_lst, 10)
+
+    print(letters)
+    return letters
+
     #draws_ten
     #is_list_of_letter_strings
     #letter_not_selected_too_many_times
