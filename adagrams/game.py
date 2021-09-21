8fc0a83c7d603049c80b5da9e115feef21328ed6
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-09-21 16:08:17 -0700

    draws letters using draw_letters function passes wave 01 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -9,7 +9,7 @@
 # |J, X                          |   8  |
 # |Q, Z                          |   10 |
 
-LETTER_POOL = {
+LETTER_VALUE = {
     'A': 1,
     'B': 3,
     'C': 3,
@@ -38,9 +38,54 @@ LETTER_POOL = {
     'Z': 10
 }
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 def draw_letters():
-    pass
+    
+    import random 
+
+    letters=[]
+    letter_pool_lst=[]
+    tup=""
+
+    for letter, numby in LETTER_POOL.items():
+        tup= tuple(letter*numby)
+
+        for t in tup:
+            for x in t:
+                letter_pool_lst.append(x)
+
+    letters = random.sample(letter_pool_lst, 10)
+
+    print(letters)
+    return letters
 
 
 def uses_available_letters(word, letter_bank):
@@ -55,7 +100,7 @@ def score_word(word):
     else:
         for letter in word:
             print(letter)
-            score_list.append(LETTER_POOL[letter.upper()])
+            score_list.append(LETTER_VALUE[letter.upper()])
             if len(score_list) >= 7:
                 score = (sum(score_list) + bonus)
             else:
