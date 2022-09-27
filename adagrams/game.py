f5230288d15ade8a29f500b6113a4077af961203
reneesu99 <reneesu22@gmail.com>
2022-09-26 20:53:57 -0700

    added wave 1 functionality

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,10 @@
+from random import sample
 def draw_letters():
-    pass
+    hand = sample(['A','B','C','D','E','F','G','H','I','J','K','L',\
+                   'M','N','O','P','Q','R','S','T','U','V','W','X',\
+                   'Y','Z'], counts = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,\
+                                       8,2,1,6,4,6,4,2,2,1,2,1], k = 10)
+    return hand
 
 def uses_available_letters(word, letter_bank):
     pass
