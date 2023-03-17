6d53b37dbd829881ba4c867569474c0798035d2b
Sarah Kim <kimsuyon2013@gmail.com>
2023-03-17 15:06:16 -0700

    test 1 and 2 of test_wave_01 passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,40 @@
+import random
 def draw_letters():
-    pass
+    list_of_letters = []
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+    for i in LETTER_POOL:
+        while len(list_of_letters) < 10:    
+            list_of_letters.append(i)
+        return list_of_letters
+        
+    
 
 def uses_available_letters(word, letter_bank):
     pass
