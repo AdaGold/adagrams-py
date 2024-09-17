389221a84bbb1a8f5546d63c670270e503b88179
Elzara Turakulova <endjik@gmail.com>
2024-09-17 15:15:31 -0700

    refactored draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,81 +2,87 @@ import random
 
 # Dictionary of the letters with their quantities
 LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
+    "A": 9,
+    "B": 2,
+    "C": 2,
+    "D": 4,
+    "E": 12,
+    "F": 2,
+    "G": 3,
+    "H": 2,
+    "I": 9,
+    "J": 1,
+    "K": 1,
+    "L": 4,
+    "M": 2,
+    "N": 6,
+    "O": 8,
+    "P": 2,
+    "Q": 1,
+    "R": 6,
+    "S": 4,
+    "T": 6,
+    "U": 4,
+    "V": 2,
+    "W": 2,
+    "X": 1,
+    "Y": 2,
+    "Z": 1,
 }
 
 # Dictionary assigning point values to the letters
 LETTERS_VALUE = {
-    'A' : 1,
-    'B' : 3,
-    'C' : 3,
-    'D' : 2,
-    'E' : 1,
-    'F' : 4,
-    'G' : 2,
-    'H' : 4,
-    'I' : 1,
-    'J' : 8,
-    'K' : 5,
-    'L' : 1,
-    'M' : 3,
-    'N' : 1,
-    'O' : 1,
-    'P' : 3,
-    'Q' : 10,
-    'R' : 1,
-    'S' : 1,
-    'T' : 1,
-    'U' : 1,
-    'V' : 4,
-    'W' : 4,
-    'X' : 8,
-    'Y' : 4,
-    'Z' : 10
+    "A": 1,
+    "B": 3,
+    "C": 3,
+    "D": 2,
+    "E": 1,
+    "F": 4,
+    "G": 2,
+    "H": 4,
+    "I": 1,
+    "J": 8,
+    "K": 5,
+    "L": 1,
+    "M": 3,
+    "N": 1,
+    "O": 1,
+    "P": 3,
+    "Q": 10,
+    "R": 1,
+    "S": 1,
+    "T": 1,
+    "U": 1,
+    "V": 4,
+    "W": 4,
+    "X": 8,
+    "Y": 4,
+    "Z": 10,
 }
+
 
 def draw_letters():
 
     # Draw 10 letters randomly, considering the quantity limits
     letter_list = list(LETTER_POOL.keys())
-    tiles = {} # Dictionary with letter and count to avoid nested loops
-    letter_bank = []
+    tiles = {}  # Dictionary with letter and count to avoid nested loops
+    letter_bank = [] 
+
     while len(letter_bank) < 10:
-        random_number = random.randint(0, 25)
+        random_number = random.randint(0, len(letter_list) - 1)
         random_letter = letter_list[random_number]
-        if random_letter not in tiles:
-            tiles[random_letter] = 1
-            letter_bank.append(random_letter)
-        elif tiles[random_letter] < LETTER_POOL[random_letter]: 
+
+        if random_letter in tiles:
+            if tiles[random_letter] >= LETTER_POOL[random_letter]:
+                continue
             tiles[random_letter] += 1
-            letter_bank.append(random_letter)
+        else:
+            tiles[random_letter] = 1
+            
+        letter_bank.append(random_letter)
 
     return letter_bank
+
 
 # Count occurrences of a letter in an iterable
 def count_items(item, iterable):
@@ -86,11 +92,14 @@ def count_items(item, iterable):
             count += 1
     return count
 
+
 def uses_available_letters(word, letter_bank):
     capital_word = word.upper()
 
     for letter in set(capital_word):
-        if letter not in letter_bank or count_items(letter, capital_word) > count_items(letter, letter_bank):
+        if letter not in letter_bank or count_items(letter, capital_word) > count_items(
+            letter, letter_bank
+        ):
             return False
     return True
 
@@ -106,6 +115,7 @@ def score_word(word):
         score += 8
     return score
 
+
 # Function to find the maximum value in a list of numbers
 def calculate_max(iterable):
     max = 0
@@ -114,11 +124,12 @@ def calculate_max(iterable):
             max = value
     return max
 
+
 # Function to find the minimum length of words in a list
 def calculate_min_length(list_of_words):
     if not list_of_words:
-        return None 
-    
+        return None
+
     min_length = len(list_of_words[0])
     for word in list_of_words:
         if len(word) < min_length:
@@ -132,10 +143,10 @@ def get_highest_word_score(word_list):
     for word in word_list:
         score = score_word(word)
         word_score[word] = score
-    
+
     # Calculate max score
     max_score = calculate_max(word_score.values())
-    
+
     # Find all candidate words with max scores
     candidates = []
     for word, score in word_score.items():
@@ -146,9 +157,9 @@ def get_highest_word_score(word_list):
     min_length = calculate_min_length(candidates)
 
     # Determine a winner considering rules
-    current_winner = ''
+    current_winner = ""
     for word in candidates:
-        if len(word) == 10: 
+        if len(word) == 10:
             current_winner = word
             break
         if len(word) == min_length and not current_winner:
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 from adagrams.ui_helper import *
-from adagrams.game import draw_letters, uses_available_letters, score_word, get_highest_word_score
+from adagrams.game import (
+    draw_letters,
+    uses_available_letters,
+    score_word,
+    get_highest_word_score,
+)
+
 
 def wave_1_run_game():
     display_welcome_message()
@@ -9,12 +15,13 @@ def wave_1_run_game():
         print("Let's draw 10 letters from the letter pool...")
         letter_bank = draw_letters()
         display_drawn_letters(letter_bank)
-    
+
         display_retry_instructions()
         continue_input = input()
         game_continue = continue_input == "y"
-   
+
     display_goodbye_message()
+
 
 def wave_2_run_game():
     display_welcome_message()
@@ -26,15 +33,16 @@ def wave_2_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while not uses_available_letters(user_input_word, letter_bank):
             display_needs_valid_input_message()
             user_input_word = input()
 
         display_retry_instructions()
         continue_input = input()
         game_continue = continue_input == "y"
-   
+
     display_goodbye_message()
+
 
 def wave_3_run_game():
     display_welcome_message()
@@ -46,10 +54,10 @@ def wave_3_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while not uses_available_letters(user_input_word, letter_bank):
             display_needs_valid_input_message()
             user_input_word = input()
-        
+
         score = score_word(user_input_word)
         display_score(score)
 
@@ -57,6 +65,7 @@ def wave_3_run_game():
         continue_input = input()
         game_continue = continue_input == "y"
     display_goodbye_message()
+
 
 def wave_4_run_game():
     display_welcome_message()
@@ -69,10 +78,10 @@ def wave_4_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while not uses_available_letters(user_input_word, letter_bank):
             display_needs_valid_input_message()
             user_input_word = input()
-        
+
         score = score_word(user_input_word)
         display_score(score)
         played_words.append(user_input_word)
@@ -83,21 +92,23 @@ def wave_4_run_game():
     display_highest_score(get_highest_word_score(played_words))
     display_goodbye_message()
 
+
 def main(wave):
-    if(wave == 1):
+    if wave == 1:
         wave_1_run_game()
-    elif(wave == 2):
+    elif wave == 2:
         wave_2_run_game()
-    elif(wave == 3):
+    elif wave == 3:
         wave_3_run_game()
-    elif(wave == 4):
+    elif wave == 4:
         wave_4_run_game()
     else:
         print("Please input a wave number.  Valid wave numbers are 1, 2, 3, 4.")
 
+
 if __name__ == "__main__":
     args = sys.argv
-    if(len(args) >= 2 and args[1].isnumeric()):
+    if len(args) >= 2 and args[1].isnumeric():
         wave = int(args[1])
     else:
         wave = "ERROR"
