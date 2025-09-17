0ac6b1a6783a74359bd797f998099e2cb59066ae
Qiusi Agre <qiusi.agre@icloud.com>
2025-09-17 11:25:35 -0400

    Drawing 10 letters randomly from the pool
    1. Build helper_function get extend the data pool
    2. Use radint and for loop to draw 10 times from
       the extended pool to get random index of the
       list of extend data pool
    3. Append the list[index] to the list of random
       drawing letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,15 +1,63 @@
 from random import randint
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+def get_draw_pool():
+    draw_pool = []
+
+    for letter, num in LETTER_POOL.items():
+        while num > 0:
+            draw_pool.append(letter)
+            num -= 1
+            # print(num)
+    return draw_pool
+
+# print(get_draw_pool())
+
+
 def draw_letters():
-    hi
-    pass
+    get_letter = []
+    draw_pool = get_draw_pool()
+    for i in range(0, 10):
+        r = randint(0, len(get_draw_pool())-1)
+        get_letter.append(draw_pool[r])
+    return get_letter
+
+# print(draw_letters())
 
 
-def uses_available_letters(word, letter_bank):
-    pass
+# def uses_available_letters(word, letter_bank):
+#     def helper_function():
 
-def score_word(word):
-    pass
+# def score_word(word):
+#     pass
 
-def get_highest_word_score(word_list):
-    pass
+# def get_highest_word_score(word_list):
+#     pass
